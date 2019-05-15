008cc047e12260304ea0065938220248a9a02e00
pjanik <janikpiotrek@gmail.com>
2019-05-15 15:01:50 +0200

    Set TS esModuleInterop option to true and update related code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Provider } from "mobx-react";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
 import { AppComponent } from "./components/app";
 import { createStores } from "./models/stores";
