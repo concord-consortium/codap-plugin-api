adb2e12d421e78bcc1634f53bb132666fb07489c
Scott Cytacki <scytacki@concord.org>
2020-11-06 16:26:37 -0500

    update deploy script to use GH actions ENV vars

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -4,17 +4,20 @@ DISTRIBUTION_ID='E1YPVV3YLYS4J7'
 # name of branch to deploy to root of site
 PRODUCTION_BRANCH='production'
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo "skipping deploy to S3: this is a pull request"
-	exit 0
-fi
-
 # extract current TAG if present
 # the 2> is to prevent error messages when no match is found
-CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+CURRENT_TAG=`git describe --tags --exact-match $GITHUB_SHA 2> /dev/null`
+
+# Extract the branch or tag name fromt he GITHUB_REF
+# it should either be: refs/head/branch-name or
+# or refs/tags/v1.2.3
+# since we ought to know if this is branch or tag based on the string we
+# we could simplfy the CURRENT_TAG approach above
+BRANCH_OR_TAG=${GITHUB_REF#refs/*/}
+echo branch or tag: $BRANCH_OR_TAG
 
 # strip PT ID from branch name for branch builds
-DEPLOY_DIR_NAME=$TRAVIS_BRANCH
+DEPLOY_DIR_NAME=$BRANCH_OR_TAG
 PT_PREFIX_REGEX="^([0-9]{8,}-)(.+)$"
 PT_SUFFIX_REGEX="^(.+)(-[0-9]{8,})$"
 if [[ $DEPLOY_DIR_NAME =~ $PT_PREFIX_REGEX ]]; then
@@ -25,16 +28,16 @@ if [[ $DEPLOY_DIR_NAME =~ $PT_SUFFIX_REGEX ]]; then
 fi
 
 # tagged builds deploy to /version/TAG_NAME
-if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
+if [ "$BRANCH_OR_TAG" = "$CURRENT_TAG" ]; then
   mkdir -p _site/version
-  S3_DEPLOY_DIR="version/$TRAVIS_BRANCH"
+  S3_DEPLOY_DIR="version/$BRANCH_OR_TAG"
   DEPLOY_DEST="_site/$S3_DEPLOY_DIR"
-  INVAL_PATH="/version/$TRAVIS_BRANCH/index.html"
+  INVAL_PATH="/version/$BRANCH_OR_TAG/index.html"
   # used by s3_website.yml
   export S3_DEPLOY_DIR
 
 # production branch builds deploy to root of site
-elif [ "$TRAVIS_BRANCH" = "$PRODUCTION_BRANCH" ]; then
+elif [ "$BRANCH_OR_TAG" = "$PRODUCTION_BRANCH" ]; then
   DEPLOY_DEST="_site"
   INVAL_PATH="/index.html"
 
