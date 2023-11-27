db051f31a6ceddb2c1cadf83d0451ca6957ee509
lublagg <lvblagg@gmail.com>
2023-11-27 12:55:07 -0500

    Add component listener.

--- src/api/codap-helper.ts
+++ src/api/codap-helper.ts
@@ -74,6 +74,10 @@ export const selectSelf = () => {
   });
 };
 
+export const addComponentListener = (callback: ClientHandler) => {
+  codapInterface.on("notify", "component", callback);
+};
+
 ////////////// data context functions //////////////
 
 export const getListOfDataContexts = async () => {
